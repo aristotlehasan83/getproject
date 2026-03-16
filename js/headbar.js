50e2c837d7809d912c9595a2227e04d958a3ed6f
aristotlehasan83 <aristotlehasan83@gmail.com>
2026-03-16 16:00:40 +0700

    Commit

--- js/headbar.js
+++ js/headbar.js
@@ -122,13 +122,4 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("resize", () => {
     if (window.innerWidth > 900) closeMenu();
   });
-
-  /* ================= STICKY HEADER (OPTIONAL) ================= */
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 20) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
 });
